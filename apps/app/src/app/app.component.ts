6e7a9cd1b3eb1aa89d2e839c48058b0c5dd7fc39
Oleksandr <oleksandrk@inflection.com>
2020-09-15 17:20:19 +0300

    change indentation

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'nx-demo-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'nx-demo-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
 }
